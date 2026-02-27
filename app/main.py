f485dcafd9c179820af5f6a0e00dd1c8e743046a
Arsh Patel <Arshpatel213@gmail.com>
2026-02-28 00:07:28 +0530

    added origin for render backend

--- app/main.py
+++ app/main.py
@@ -66,12 +66,12 @@ async def add_security_headers(request: Request, call_next):
 
 app.add_middleware(SlowAPIMiddleware)
 
-# Get allowed origins from environment variable
-origins = ["https://job-tracker-sa35.vercel.app/"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+     allow_origins=[
+        "http://localhost:3000",
+        "https://job-tracker-sa35.vercel.app"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["Authorization", "Content-Type"],
